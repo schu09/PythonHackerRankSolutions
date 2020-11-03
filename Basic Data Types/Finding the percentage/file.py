6ee516786b50e1dc2d712a20188c653bf2fd4995
Sebastian Chu <45053583+schu09@users.noreply.github.com>
2020-11-03 13:46:00 -0500

    :D

---
+++ Basic Data Types/Finding the percentage/file.py
@@ -0,0 +1,7 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
