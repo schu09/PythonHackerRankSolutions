ffd1774d2b506cc82cf7a811ebe59529d4504ccd
Sebastian Chu <45053583+schu09@users.noreply.github.com>
2020-11-03 13:22:52 -0500

    :D

---
+++ Introduction/Python: Division/file.py
@@ -0,0 +1,11 @@
+# stores a and b
+a = int(input())
+b = int(input())
+
+# creates and assigns divisions
+intDivision = (a // b)
+floatDivision= (a / b)
+
+# prints values
+print(intDivision)
+print(floatDivision)
